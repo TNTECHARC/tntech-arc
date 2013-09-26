110f09458cb296e8172aba692e57d4df8582cee6
cvchaparro <cvchaparro@gmail.com>
2013-09-26 20:04:19 +0000

    Made stuff look prettier

--- asimov/messages/message_ids.py
+++ asimov/messages/message_ids.py
@@ -1,14 +1,13 @@
-
 #direct copy of message_ids.h
-msg_Empty           = 0
-msg_Command_ID      = 2    #{ msg_Command }
-msg_ClientLogin_ID  = 3    #{ msg_ClientLogin }
-msg_ServerAccept_ID = 4    #{ msg_ServerAccept }
-msg_Echo_ID         = 5    #{ plain ascii text }
-msg_LaserScan_ID    = 6    #{ msg_LaserScan }
-msg_LidarPotential_ID = 7  #{ msg_LaserScan }
-msg_RobotPosition_ID = 8   #{ msg_Position }
-msg_RobotWaypoint_ID = 9   #{ msg_Position }
-msg_GPS_ID          = 10   #{ msg_GPS }
-msg_IMU_ID          = 11   #{ msg_IMU }
+msg_Empty             = 0
+msg_Command_ID        = 2    #{ msg_Command }
+msg_ClientLogin_ID    = 3    #{ msg_ClientLogin }
+msg_ServerAccept_ID   = 4    #{ msg_ServerAccept }
+msg_Echo_ID           = 5    #{ plain ascii text }
+msg_LaserScan_ID      = 6    #{ msg_LaserScan }
+msg_LidarPotential_ID = 7    #{ msg_LaserScan }
+msg_RobotPosition_ID  = 8    #{ msg_Position }
+msg_RobotWaypoint_ID  = 9    #{ msg_Position }
+msg_GPS_ID            = 10   #{ msg_GPS }
+msg_IMU_ID            = 11   #{ msg_IMU }
 
